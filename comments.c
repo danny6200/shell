d113f1afae19b09a9a42125add8454513f1c91e3
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-08-21 07:28:29 +0000

    added comments.c

--- comments.c
+++ comments.c
@@ -6,33 +6,33 @@
  */
 void comment(char *arr[])
 {
-        int i = 0, n;
+	int i = 0, n;
 
 
 	if (arr[0][0] == '#')
 		exit(0);
-        while (arr[i] != NULL)
-        {
-                if (_strcmp(arr[i], "#") == 0)
-                {
-                        arr[i] = NULL;
-                        return;
-                }
-                for (n = 0; arr[i][n] != '\0'; n++)
-                {
-                        if (arr[i][n] == '#')
-                        {
+	while (arr[i] != NULL)
+	{
+		if (_strcmp(arr[i], "#") == 0)
+		{
+			arr[i] = NULL;
+			return;
+		}
+		for (n = 0; arr[i][n] != '\0'; n++)
+		{
+			if (arr[i][n] == '#')
+			{
 				if (arr[i][n - 1] != ' ')
 					return;
-                                arr[i][n] = '\0';
-                                if (arr[i + 1] != NULL)
-                                {
-                                        arr[i + 1] = NULL;
-                                        return;
-                                }
-                                return;
-                        }
-                }
-                i++;
-        }
+				arr[i][n] = '\0';
+				if (arr[i + 1] != NULL)
+				{
+					arr[i + 1] = NULL;
+					return;
+				}
+				return;
+			}
+		}
+		i++;
+	}
 }
